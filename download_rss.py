22cbc4b4745fde560de90aac53e488963bbf8513
root <root@mainframe.local>
2023-04-09 12:07:48 +0300

    messing with other tools

--- download_rss.py
+++ download_rss.py
@@ -1,10 +1,19 @@
 # import all the shit
 import requests
+import feedparser
 from RSS_Sources import RSS_FEED_LIST
 
 # do the things
 print(RSS_FEED_LIST['Pauls_Hardware'])
 print('\r\n')
 
-for URL in RSS_FEED_LIST:
-    print(RSS_FEED_LIST[URL])
+print(feedparser.parse(RSS_FEED_LIST['Pauls_Hardware']))
+
+
+# RSS_CONTENT = requests.get(RSS_FEED_LIST['Pauls_Hardware'])
+# print(RSS_CONTENT.status_code)
+# print(RSS_CONTENT.content)
+
+
+# for URL in RSS_FEED_LIST:
+#     print(RSS_FEED_LIST[URL])
