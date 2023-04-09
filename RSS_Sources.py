3ae65510217ec551f7935cb1bf2cdb066cd421fc
root <root@mainframe.local>
2023-04-09 12:01:54 +0300

    i am stupid

---
+++ RSS_Sources.py
@@ -0,0 +1,4 @@
+RSS_FEED_LIST = {
+    'Pauls_Hardware':"https://www.youtube.com/feeds/videos.xml?channel_id=UCvWWf-LYjaujE50iYai8WgQ",
+    'Gareth_Dennis':"https://www.youtube.com/feeds/videos.xml?channel_id=UCzA-8fUrw2C5cRcP9gO5BwA"
+}
--- download_rss.py
+++ download_rss.py
@@ -1,1 +1,3 @@
-from RSS-Sources import 
+from RSS_Sources import RSS_FEED_LIST
+
+print(RSS_FEED_LIST['Pauls_Hardware'])
