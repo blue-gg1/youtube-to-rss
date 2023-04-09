37726b7f971f96314590154e80ad802c469987d3
root <root@mainframe.local>
2023-04-09 11:57:56 +0300

    messing with the rss list

---
+++ RSS-Sources.py
@@ -0,0 +1,2 @@
+Pauls_Hardware = "https://www.youtube.com/feeds/videos.xml?channel_id=UCvWWf-LYjaujE50iYai8WgQ"
+Gareth_Dennis = "https://www.youtube.com/feeds/videos.xml?channel_id=UCzA-8fUrw2C5cRcP9gO5BwA"
