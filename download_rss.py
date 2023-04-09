eda15d7bcfbc1c9e531ffe8cd871d7e38bf3a6fd
root <root@mainframe.local>
2023-04-09 12:17:05 +0300

    failing to get regex to work

--- download_rss.py
+++ download_rss.py
@@ -1,13 +1,21 @@
 # import all the shit
 import requests
 import feedparser
+import re
 from RSS_Sources import RSS_FEED_LIST
 
 # do the things
 print(RSS_FEED_LIST['Pauls_Hardware'])
 print('\r\n')
 
-print(feedparser.parse(RSS_FEED_LIST['Pauls_Hardware']))
+# feed_content = feedparser.parse(RSS_FEED_LIST['Pauls_Hardware']).keys()
+# feed_content_str = str(feed_content)
+
+RSS_CONTENT = requests.get(RSS_FEED_LIST['Pauls_Hardware'])
+RSS_CONTENT_STR = str(RSS_CONTENT)
+print(RSS_CONTENT.status_code)
+
+print(re.findall("https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)", RSS_CONTENT_STR))
 
 
 # RSS_CONTENT = requests.get(RSS_FEED_LIST['Pauls_Hardware'])
