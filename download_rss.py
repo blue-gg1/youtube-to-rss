aadc01dfde100b91fc64e4ae0d2169852b07ec73
root <root@mainframe.local>
2023-04-09 12:21:00 +0300

    regex is failing

--- download_rss.py
+++ download_rss.py
@@ -16,8 +16,9 @@ RSS_CONTENT_STR = str(RSS_CONTENT.content)
 print(RSS_CONTENT.status_code)
 
 # print(RSS_CONTENT_STR)
+URL_REGEX =  "https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)"
 
-URLS = re.findall("https", RSS_CONTENT_STR)
+URLS = re.findall(URL_REGEX, RSS_CONTENT_STR)
 print(URLS)
 
 
