a662668d5e63067bd4b5bd5816dd4a9441874363
root <root@mainframe.local>
2023-04-09 12:04:47 +0300

    making a loop and so on

--- download_rss.py
+++ download_rss.py
@@ -1,3 +1,10 @@
+# import all the shit
+import requests
 from RSS_Sources import RSS_FEED_LIST
 
+# do the things
 print(RSS_FEED_LIST['Pauls_Hardware'])
+print('\r\n')
+
+for URL in RSS_FEED_LIST:
+    print(RSS_FEED_LIST[URL])
